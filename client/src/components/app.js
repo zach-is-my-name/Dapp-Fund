554451ef4a8093b4bb30d91676ff2db3f4e076fe
John Kneafsey <johnfkneafsey@gmail.com>
2017-03-24 15:06:27 -0400

    need to fix back end connection

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,50 +1,22 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
 
-import QuestionPage from './question-page';
-import LoginPage from './login-page';
+import MemberDapps from './MemberDapps';
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            currentUser: null
-        };
     }
 
     componentDidMount() {
-        // Job 4: Redux-ify all of the state and fetch calls to async actions.
-        const accessToken = Cookies.get('accessToken');
-        if (accessToken) {
-            fetch('/api/me', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }).then(res => {
-                if (!res.ok) {
-                    if (res.status !== 401) {
-                        // Unauthorized, clear the cookie and go to
-                        // the login page
-                        Cookies.remove('accessToken');
-                        return;
-                    }
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            }).then(currentUser =>
-                this.setState({
-                    currentUser
-                })
-            );
-        }
+      
     }
 
     render() {
-        if (!this.state.currentUser) {
-            return <LoginPage />;
-        }
 
-        return <QuestionPage />;
+        return <MemberDapps />;
+
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,82 +1,10 @@
 const path = require('path');
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const BearerStrategy = require('passport-http-bearer').Strategy;
-
-const secret = require('./secret');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const app = express();
 
-const database = {
-};
-
-app.use(passport.initialize());
-
-passport.use(
-    new GoogleStrategy({
-        clientID:  '689026946763-rtsrhg52nra9oai4tk7gb05fs8f1t43l.apps.googleusercontent.com',
-        clientSecret: secret,
-        callbackURL: `/api/auth/google/callback`
-    },
-    (accessToken, refreshToken, profile, cb) => {
-        // Job 1: Set up Mongo/Mongoose, create a User model which store the
-        // google id, and the access token
-        // Job 2: Update this callback to either update or create the user
-        // so it contains the correct access token
-        const user = database[accessToken] = {
-            googleId: profile.id,
-            accessToken: accessToken
-        };
-        return cb(null, user);
-    }
-));
-
-passport.use(
-    new BearerStrategy(
-        (token, done) => {
-            // Job 3: Update this callback to try to find a user with a 
-            // matching access token.  If they exist, let em in, if not,
-            // don't.
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
-        }
-    )
-);
-
-app.get('/api/auth/google',
-    passport.authenticate('google', {scope: ['profile']}));
-
-app.get('/api/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
-    }),
-    (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
-    }
-);
-
-app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
-});
-
-app.get('/api/me',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json({
-        googleId: req.user.googleId
-    })
-);
-
-app.get('/api/questions',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json(['Question 1', 'Question 2'])
-);
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -87,6 +15,291 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
     const index = path.resolve(__dirname, '../client/build', 'index.html');
     res.sendFile(index);
 });
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host:'stampy.db.elephantsql.com',
+        port:5432,
+        ssl:true,
+        user: 'nyfibjdb',
+        password:'rfdZ6HnpaOAP8bKSqzxgN33hx1Z4jP2O',
+        database:'nyfibjdb',
+        url: 'postgres://nyfibjdb:rfdZ6HnpaOAP8bKSqzxgN33hx1Z4jP2O@stampy.db.elephantsql.com:5432/nyfibjdb',
+    },
+});
+// User submitting application
+app.post('/users', jsonParser, (req, res) => {
+    let userName = req.body.userName;
+    let etherAddress = req.body.etherAddress;
+    let confirmedEtherFeeTransactionAddress = req.body.confirmedEtherFeeTransactionAddress;
+    let dappName = req.body.dappName;
+    let dappDescription = req.body.dappDescription;
+    let dappImageLink = req.body.dappImageLink;
+    let dappEtherAddress = req.body.dappEtherAddress;
+    let status = "pending";
+    // Now use create Knex function to create user in DB
+    knex.insert({
+        username: userName,
+        useretheraddress: etherAddress,
+        entryfeetransaction: confirmedEtherFeeTransactionAddress,
+        dappname: dappName,
+        dappdescription: dappDescription,
+        dappimagelink: dappImageLink,
+        dappetheraddress: dappEtherAddress,
+        memberstatus: status
+    }).into('users').then(user => {
+        console.log("user created: ", user);
+        res.status(201).json(user);
+    })
+    
+})
+// TO get list of Dapps for frontend and their details
+app.get('/users', (req, res) => {
+    knex.select('id', 'username', 'useretheraddress', 'entryfeetransaction', 'dappname', 'dappdescription', 'dappimagelink', 'dappetheraddress')
+    .from('users')
+    .where({memberstatus: 'confirmed'})
+    .orderBy('id')
+    .then(users => {
+        res.status("200").json(users);
+    })    
+})
+// Owner approving a user
+app.put('/users/:userID', jsonParser, (req, res) => {
+  let userID = parseInt(req.params.userID);
+  console.log("userID", userID);
+  knex('users')
+    .update({ memberstatus: "confirmed" })
+    .where({id: userID})
+    .then(user => {
+      console.log("updated user: ", user);
+      res.status(201).json(user);
+    })
+})
+// User submitting proposal
+app.post('/proposals', jsonParser, (req, res) => {
+    let userName = req.body.userName;
+    let proposalDescription = req.body.proposalDescription;
+    let proposedFunding = req.body.proposedFunding;
+    let yesVotes = 0;
+    let noVotes = 0;
+    let dateCreated = new Date();
+    knex.select('id')
+      .from('users')
+      .where({username: userName})
+      .then(user => {
+        console.log('is this correct user id?', user[0].id);
+        let userID = user[0].id;
+        knex.insert({
+          user_id: userID,
+          proposaldescription: proposalDescription,
+          proposedfunding: proposedFunding,
+          yesvotes: yesVotes,
+          novotes: noVotes,
+          datecreated: dateCreated
+        })
+        .into('proposals')
+        .then(proposal => {
+          console.log("new proposal", proposal);
+          res.status(201).json(proposal);
+        })
+      })
+})
+// To list of proposed Dapps
+app.get('/proposals', (req, res) => {
+  function timeRemaining(inputDate) {
+    console.log("inputDate", inputDate);
+    let year = inputDate.slice(11,15);
+    let month = (inputDate.slice(4,7));
+    if (month === "Jan") {
+      month = 0;
+    } else if (month === "Feb") {
+      month = 1;
+    } else if (month === "Mar") {
+      month = 2;
+    } else if (month === "Apr") {
+      month = 3;
+    } else if (month === "May") {
+      month = 4;
+    } else if (month === "Jun") {
+      month = 5;
+    } else if (month === "Jul") {
+      month = 6;
+    } else if (month === "Aug") {
+      month = 7;
+    } else if (month === "Sep") {
+      month = 8;
+    } else if (month === "Oct") {
+      month = 9;
+    } else if (month === "Nov") {
+      month = 10;
+    } else if (month === "Dec") {
+      month = 11;
+    }
+    let day = inputDate.slice(8,10);
+    let hour = inputDate.slice(16,18);
+    let minute = inputDate.slice(19,21);
+    let d = new Date(year, month, day, hour, minute, 00);
+    let currentDate = new Date();
+    let diff = new Date(currentDate.getTime() - d.getTime());
+    let years = diff.getUTCFullYear() - 1970; // Gives difference as year
+    let months = diff.getUTCMonth(); // Gives month count of difference
+    let days = diff.getUTCDate()-1; // Gives day count of difference
+    let hours = diff.getUTCHours(); // Gives hour count of difference
+    let minutes = diff.getUTCMinutes(); // Gives minute count of difference
+    let remainingDays = 4 - days;
+    let remainingHours = 23 - hours;
+    let remainingMinutes = 60 - minutes;
+  let remainingTime;
+    if (remainingDays > 0 || remainingHours > 0 || remainingMinutes > 0) {
+      remainingTime = `${remainingDays} days, ${remainingHours} hours, ${remainingMinutes} minutes before voting deadline.`;
+    } else {
+      remainingTime = 'Expired';
+    }
+    
+    return remainingTime;
+  }
+    knex.select(['proposals.id', 'proposals.proposaldescription', 'proposals.proposedfunding', 'proposals.yesvotes', 'proposals.novotes', 'proposals.datecreated', 'proposals.executed', 'users.username', 'users.dappname', 'users.dappdescription', 'users.dappimagelink'])
+    .from('users')
+    .join('proposals', 'proposals.user_id', 'users.id')
+    .then(proposals => {
+      console.log("proposal list: ", proposals);
+      let updatedProposals = proposals.map(proposal => {
+        console.log("proposal.datecreated", proposal.datecreated);
+        let o = Object.assign({}, proposal);
+        o.timeLeft = timeRemaining(String(proposal.datecreated));
+        return o;
+      })
+      return updatedProposals;
+    })
+    .then(results => {
+        console.log("proposal list: ", results);
+        res.status(200).json(results);
+      })
+  })
+//To vote on proposal
+app.put('/proposals/:id/:value', (req, res) => {
+  let proposalID = req.params.id;
+  let vote = req.params.value;
+  if (vote === "yes") {
+  
+    knex.select('yesvotes').from('proposals')
+      .where({ id: proposalID })
+      .then(result => {
+        let voteCount = result[0].yesvotes;
+        return voteCount;
+      })
+      .then(_res => {
+        console.log('still see proposalID?', proposalID);
+        let newCount = _res + 1;
+        console.log('new count', newCount);
+        knex('proposals')
+          .update({ yesvotes: newCount })
+          .where({id: proposalID })
+          .then(voted => {
+            res.status(200).send("voted!")
+          })
+      })
+  } else if (vote === "no") {
+      knex.select('novotes').from('proposals')
+      .where({ id: proposalID })
+      .then(result => {
+        let voteCount = result[0].novotes;
+        return voteCount;
+      })
+      .then(_res => {
+        console.log('still see proposalID?', proposalID);
+        let newCount = _res + 1;
+        console.log('new count', newCount);
+        knex('proposals')
+          .update({ novotes: newCount })
+          .where({id: proposalID })
+          .then(voted => {
+            res.status(200).send("voted!")
+          })
+      })
+    } else {
+      res.status(400)
+    }
+    
+})
+//To execute proposal
+app.put('/execute/:id', (req, res) => {
+function timeRemaining(inputDate) {
+    console.log("inputDate", inputDate);
+    let year = inputDate.slice(11,15);
+    let month = (inputDate.slice(4,7));
+    if (month === "Jan") {
+      month = 0;
+    } else if (month === "Feb") {
+      month = 1;
+    } else if (month === "Mar") {
+      month = 2;
+    } else if (month === "Apr") {
+      month = 3;
+    } else if (month === "May") {
+      month = 4;
+    } else if (month === "Jun") {
+      month = 5;
+    } else if (month === "Jul") {
+      month = 6;
+    } else if (month === "Aug") {
+      month = 7;
+    } else if (month === "Sep") {
+      month = 8;
+    } else if (month === "Oct") {
+      month = 9;
+    } else if (month === "Nov") {
+      month = 10;
+    } else if (month === "Dec") {
+      month = 11;
+    }
+    let day = inputDate.slice(8,10);
+    let hour = inputDate.slice(16,18);
+    let minute = inputDate.slice(19,21);
+    let d = new Date(year, month, day, hour, minute, 00);
+    let currentDate = new Date();
+    let diff = new Date(currentDate.getTime() - d.getTime());
+    let years = diff.getUTCFullYear() - 1970; // Gives difference as year
+    let months = diff.getUTCMonth(); // Gives month count of difference
+    let days = diff.getUTCDate()-1; // Gives day count of difference
+    let hours = diff.getUTCHours(); // Gives hour count of difference
+    let minutes = diff.getUTCMinutes(); // Gives minute count of difference
+    let remainingDays = 4 - days;
+    let remainingHours = 23 - hours;
+    let remainingMinutes = 60 - minutes;
+  let remainingTime;
+    if (remainingDays > 0 || remainingHours > 0 || remainingMinutes > 0) {
+      remainingTime = `${remainingDays} days, ${remainingHours} hours, ${remainingMinutes} minutes before voting deadline.`;
+    } else {
+      remainingTime = 'Expired';
+    }
+    
+    return remainingTime;
+  }
+  let proposalID = req.params.id;
+  knex.select('yesvotes', 'novotes', 'datecreated', 'executed')
+    .from('proposals')
+    .where({id: proposalID})
+    .then(result => {
+      let proposal = result[0];
+      let proposaleDate = proposal.datecreated
+      console.log("proposaleDate?", proposaleDate);
+      console.log("date to string", String(proposaleDate));
+      console.log(timeRemaining(String(proposaleDate)));
+      
+      if(proposal.executed === 'false' && timeRemaining(String(proposaleDate)) === 'Expired' && proposal.yesvotes > proposal.novotes) {
+        knex('proposals')
+          .update({executed: 'true'})
+          .where({id: proposalID})
+          .then(_res => {
+            // console.log("_res", _res);
+            res.status(200).send("propsoal executed.")
+          })
+      } else {
+        res.status(406).send("Could not execute");
+      }
+    })
+})
 
 let server;
 function runServer(port=3001) {

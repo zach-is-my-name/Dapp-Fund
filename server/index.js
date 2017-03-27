0ff5c69a4dbda034c9686dd87997e7587ec2fcd1
Alex M <aljmiller87@gmail.com>
2017-03-27 15:28:42 -0400

    cont'd

--- server/index.js
+++ server/index.js
@@ -3,18 +3,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+// const config = require('./config');
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', config.CLIENT_ROOT);
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     next();
+// });
+
 const app = express();
 
 
 // Serve the built client
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// // Unhandled requests which aren't for the API should serve index.html so
-// // client-side routing using browserHistory can function
-// app.get(/^(?!\/api(\/|$))/, (req, res) => {
-//     const index = path.resolve(__dirname, '../client/build', 'index.html');
-//     res.sendFile(index);
-// });
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+} 
+
+app.use(allowCrossDomain);
+
+
+
 const knex = require('knex')({
     client: 'pg',
     connection: {

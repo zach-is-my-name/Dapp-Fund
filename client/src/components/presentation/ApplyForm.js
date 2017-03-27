72ca8adf91d7ced6b99d85e0ab0538f1b8631af7
zach-is-my-name <zach.greco@gmail.com>
2017-03-27 12:12:05 -0400

    Apply Form working

--- client/src/components/presentation/ApplyForm.js
+++ client/src/components/presentation/ApplyForm.js
@@ -7,22 +7,36 @@ export class ApplyForm extends React.Component {
   constructor(props) {
     super(props);
     this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      username: '',
+      useretheraddress: '',
+      entryfeetransaction: '',
+      dappname: '',
+      dappname: '',
+      dappdescription: '',
+      dappimagelink:'',
+      dappetheraddress:'',
+      memberstatus:'pending',
+  }
   }
 
+handleChange(event) {
+this.setState({[event.target.name]: event.target.value})
+}
 
 submit(event) {
 let applyObj = {
-    username:this.refs.dappCreator.value ,
-    useretheraddress:this.refs.creatorAddress.value,
-    entryfeetransaction:this.refs.membershipHash.value ,
-    dappname: this.refs.dappName.value,
-    dappdescription: this.refs.dappDescription.value,
-    dappimagelink: this.refs.imageLink.value,
-    dappetheraddress: this.refs.dappLink.value ,
-    memberstatus: "pending"
+    username:this.state.username ,
+    useretheraddress:this.state.useretheraddress,
+    entryfeetransaction:this.state.entryfeetransaction,
+    dappname: this.state.dappname,
+    dappdescription: this.state.dappdescription,
+    dappimagelink: this.state.dappimagelink,
+    dappetheraddress: this.state.dappetheraddress,
+    memberstatus: this.state.memberstatus,
   }
   console.log("APPLY OBJ", applyObj)
-  console.log(event.target.name.enterDapp)
 this.props.dispatch(actions.submitApply(applyObj));
   event.preventDefault();
 }
@@ -32,31 +46,31 @@ this.props.dispatch(actions.submitApply(applyObj));
       <Form onSubmit={this.submit}>
         <FormGroup>
           <Label for="dapp-name">Dapp Name</Label>
-          <Input type="text" name="enterDapp" id="enterDapp" placeholder="Enter Dapp Name"  required/>
+          <Input type="text" value={this.state.dappname} name="dappname"  id="enterDapp"onChange={this.handleChange} placeholder="Enter Dapp Name"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="dapp-description">Dapp Description</Label>
-          <Input type="text" name="dappDescription" id="dappDescription" placeholder="Enter Dapp Description" ref="dappDescription" required/>
+          <Input type="text" value={this.state.dappdescription} name="dappdescription" id="dappDescription"onChange={this.handleChange} placeholder="Enter Dapp Description"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="dapp-image-link"> Dapp Image Link</Label>
-          <Input type="text" name="dappImage" id="dappImage" placeholder="Enter Dapp Image Link" ref="imageLink" required/>
+          <Input type="text" value={this.state.dappimagelink} name="dappimagelink" id="dappImage"onChange={this.handleChange} placeholder="Enter Dapp Image Link"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="dapp-link"> Dapp Link</Label>
-          <Input type="text" name="dappLink" id="dappLink" placeholder="Enter Dapp Link" ref="dappLink" required/>
+          <Input type="text" value={this.state.dappetheraddress} name="dappetheraddress" id="dappLink"onChange={this.handleChange} placeholder="Enter Dapp Link"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="dapp-creator"> Dapp Creator</Label>
-          <Input type="text" name="dappCreator" id="dappCreator" placeholder="Enter Dapp Creator Name" ref="dappCreator" required/>
+          <Input type="text" value={this.state.username} name="username" id="dappCreator"onChange={this.handleChange} placeholder="Enter Dapp Creator Name"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="dapp-creator-address"> Dapp Creator Public Key</Label>
-          <Input type="text" name="dappCreatorAddress" id="dappCreatorAddress" placeholder="Enter Dapp Creator Public Key" ref="creatorAddress" required/>
+          <Input type="text" value={this.state.useretheraddress} name="useretheraddress" id="dappCreatorAddress"onChange={this.handleChange} placeholder="Enter Dapp Creator Public Key"  required/>
         </FormGroup>
         <FormGroup>
           <Label for="membership-hash"> Membership Hash</Label>
-          <Input type="text" name="membershipHash" id="membershipHash" placeholder="Enter Membership Transaction ID" ref="membershipHash" required/>
+          <Input type="text"value={this.state.entryfeetransaction} name="entryfeetransaction" id="membershipHash"onChange={this.handleChange} placeholder="Enter Membership Transaction ID"  required/>
         </FormGroup>
         <Input type="submit" name="submit" id="apply-submit" value="Submit"/>
       </Form>

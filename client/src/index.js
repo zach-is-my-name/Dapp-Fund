f24fe8a2c71db2a91dec8bd49f949b047aeb2ec0
John Kneafsey <johnfkneafsey@gmail.com>
2017-03-30 10:42:14 -0400

    component updates

--- client/src/index.js
+++ client/src/index.js
@@ -11,19 +11,19 @@ import MemberDapps from './components/container/MemberDapps';
 import ActiveProposals from './components/container/ActiveProposals';
 import SubmitProposal from './components/container/SubmitProposal';
 
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 document.addEventListener('DOMContentLoaded', () =>
     ReactDOM.render(
         <Provider store={store} >
             <MuiThemeProvider>
             <Router history={browserHistory}>
-                <Route exact path="/" component={About} ></Route>
-                <Route exact path="/about" component={About} ></Route>
-                <Route exact path="/apply" component={Apply} />
-                <Route exact path="/memberdapps" component={MemberDapps} ></Route>
-                <Route exact path="/activeproposals" component={ActiveProposals} ></Route>
-                <Route exact path="/submitproposal" component={SubmitProposal} ></Route>
+                <Route path="/" component={About} ></Route>
+                <Route path="/about" component={About} ></Route>
+                <Route path="/apply" component={Apply} />
+                <Route path="/memberdapps" component={MemberDapps} ></Route>
+                <Route path="/activeproposals" component={ActiveProposals} ></Route>
+                <Route path="/submitproposal" component={SubmitProposal} ></Route>
             </Router>
             </MuiThemeProvider>   
         </Provider>, document.getElementById('root')
